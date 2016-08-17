6449f722409b3fe692bb7088278d747c0f730b27
Andrei Rudenko <rudenkopaint@gmail.com>
2016-08-18 02:32:29 +0300

    added wrenGetListCount and wrenAbortFiber

--- wren/Wren.hx
+++ wren/Wren.hx
@@ -11,108 +11,117 @@ import wren.WrenHandle;
 #end
 extern class Wren {
 
-    @:native('linc::wren::newVM')
-    static function newVM(?config:WrenConfiguration) : WrenVM;
+	@:native('linc::wren::newVM')
+	static function newVM(?config:WrenConfiguration) : WrenVM;
 
-    @:native('wrenFreeVM')
-    static function freeVM(vm:WrenVM) : Void;
+	@:native('wrenFreeVM')
+	static function freeVM(vm:WrenVM) : Void;
 
-    @:native('wrenCollectGarbage')
-    static function collectGarbage(vm:WrenVM) : Void;
+	@:native('wrenCollectGarbage')
+	static function collectGarbage(vm:WrenVM) : Void;
 
-    @:native('wrenInterpret')
-    static function interpret(vm:WrenVM, source:String) : WrenInterpretResult;
+	@:native('wrenInterpret')
+	static function interpret(vm:WrenVM, source:String) : WrenInterpretResult;
 
-    @:native('wrenMakeCallHandle')
-    static function makeCallHandle(vm:WrenVM, signature:String) : WrenHandle;
+	@:native('wrenMakeCallHandle')
+	static function makeCallHandle(vm:WrenVM, signature:String) : WrenHandle;
 
-    @:native('wrenCall')
-    static function call(vm:WrenVM, method:WrenHandle) : WrenInterpretResult;
+	@:native('wrenCall')
+	static function call(vm:WrenVM, method:WrenHandle) : WrenInterpretResult;
 
-    @:native('wrenReleaseHandle')
-    static function releaseHandle(vm:WrenVM, value:WrenHandle) : Void;
+	@:native('wrenReleaseHandle')
+	static function releaseHandle(vm:WrenVM, value:WrenHandle) : Void;
 
-    @:native('wrenGetSlotCount')
-    static function getSlotCount(vm:WrenVM) : Int;
+	@:native('wrenGetSlotCount')
+	static function getSlotCount(vm:WrenVM) : Int;
 
-    @:native('wrenEnsureSlots')
-    static function ensureSlots(vm:WrenVM, numSlots:Int) : Void;
+	@:native('wrenEnsureSlots')
+	static function ensureSlots(vm:WrenVM, numSlots:Int) : Void;
 
-    @:native('wrenGetSlotType')
-    static function getSlotType(vm:WrenVM, slot:Int) : WrenType;
+	@:native('wrenGetSlotType')
+	static function getSlotType(vm:WrenVM, slot:Int) : WrenType;
 
-    @:native('wrenGetSlotBool')
-    static function getSlotBool(vm:WrenVM, slot:Int) : Bool;
+	@:native('wrenGetSlotBool')
+	static function getSlotBool(vm:WrenVM, slot:Int) : Bool;
 
-    @:native('wrenGetSlotBytes')
-    static function getSlotBytes(vm:WrenVM, slot:Int, length:Int) : Bool;
+	@:native('wrenGetSlotBytes')
+	static function getSlotBytes(vm:WrenVM, slot:Int, length:Int) : Bool;
 
-    @:native('wrenGetSlotDouble')
-    static function getSlotDouble(vm:WrenVM, slot:Int) : Float;
+	@:native('wrenGetSlotDouble')
+	static function getSlotDouble(vm:WrenVM, slot:Int) : Float;
 
-    @:native('wrenGetSlotForeign')
-    static function getSlotForeign(vm:WrenVM, slot:Int) : Void;
+	@:native('wrenGetSlotForeign')
+	static function getSlotForeign(vm:WrenVM, slot:Int) : Void;
 
-    @:native('linc::wren::getSlotString')
-    static function getSlotString(vm:WrenVM, slot:Int) : String;
+	@:native('linc::wren::getSlotString')
+	static function getSlotString(vm:WrenVM, slot:Int) : String;
 
-    @:native('wrenGetSlotHandle')
-    static function getSlotHandle(vm:WrenVM, slot:Int) : WrenHandle;
+	@:native('wrenGetSlotHandle')
+	static function getSlotHandle(vm:WrenVM, slot:Int) : WrenHandle;
 
-    @:native('wrenSetSlotBool')
-    static function setSlotBool(vm:WrenVM, slot:Int, value:Bool) : Void;
+	@:native('wrenSetSlotBool')
+	static function setSlotBool(vm:WrenVM, slot:Int, value:Bool) : Void;
 
-    @:native('wrenSetSlotBytes')
-    static function setSlotBytes(vm:WrenVM, slot:Int, bytes:String, length:UInt) : Void;
+	@:native('wrenSetSlotBytes')
+	static function setSlotBytes(vm:WrenVM, slot:Int, bytes:String, length:UInt) : Void;
 
-    @:native('wrenSetSlotDouble')
-    static function setSlotDouble(vm:WrenVM, slot:Int, value:Float) : Void;
+	@:native('wrenSetSlotDouble')
+	static function setSlotDouble(vm:WrenVM, slot:Int, value:Float) : Void;
 
-    @:native('wrenSetSlotNewForeign')
-    static function setSlotNewForeign(vm:WrenVM, slot:Int, classSlot:Int, size:UInt) : Void;
+	@:native('wrenSetSlotNewForeign')
+	static function setSlotNewForeign(vm:WrenVM, slot:Int, classSlot:Int, size:UInt) : Void;
 
-    @:native('wrenSetSlotNewList')
-    static function setSlotNewList(vm:WrenVM, slot:Int) : Void;
+	@:native('wrenSetSlotNewList')
+	static function setSlotNewList(vm:WrenVM, slot:Int) : Void;
 
-    @:native('wrenSetSlotNull')
-    static function setSlotNull(vm:WrenVM, slot:Int) : Void;
+	@:native('wrenSetSlotNull')
+	static function setSlotNull(vm:WrenVM, slot:Int) : Void;
 
-    @:native('wrenSetSlotString')
-    static function setSlotString(vm:WrenVM, slot:Int, text:String) : Void;
+	@:native('wrenSetSlotString')
+	static function setSlotString(vm:WrenVM, slot:Int, text:String) : Void;
 
-    @:native('wrenSetSlotHandle')
-    static function setSlotHandle(vm:WrenVM, slot:Int, value:WrenHandle) : Void;
+	@:native('wrenSetSlotHandle')
+	static function setSlotHandle(vm:WrenVM, slot:Int, value:WrenHandle) : Void;
 
-    @:native('wrenInsertInList')
-    static function insertInList(vm:WrenVM, listSlot:Int, index:Int, elementSlot:Int) : Void;
+	@:native('wrenGetListCount')
+	static function getListCount(vm:WrenVM, slot:Int) : Int;
 
-    @:native('wrenGetVariable')
-    static function getVariable(vm:WrenVM, module:String, name:String, slot:Int) : Void;
+	@:native('wrenGetListElement')
+	static function getListElement(vm:WrenVM, listSlot:Int, index:Int, elementSlot:Int) : Void;
 
+	@:native('wrenInsertInList')
+	static function insertInList(vm:WrenVM, listSlot:Int, index:Int, elementSlot:Int) : Void;
+
+	@:native('wrenGetVariable')
+	static function getVariable(vm:WrenVM, module:String, name:String, slot:Int) : Void;
+
+	@:native('wrenAbortFiber')
+	static function abortFiber(vm:WrenVM, slot:Int) : Void;
+	
 } //Wren
 
 typedef WrenConfiguration = {
-    @:optional var initialHeapSize : UInt;
-    @:optional var minHeapSize : UInt;
-    @:optional var heapGrowthPercent : Int;
+	@:optional var initialHeapSize : UInt;
+	@:optional var minHeapSize : UInt;
+	@:optional var heapGrowthPercent : Int;
 } //WrenConfiguration
 
 @:enum
 abstract WrenInterpretResult(Int)
 from Int to Int {
-    var WREN_RESULT_SUCCESS        = 0;
-    var WREN_RESULT_COMPILE_ERROR  = 1;
-    var WREN_RESULT_RUNTIME_ERROR  = 2;
+	var WREN_RESULT_SUCCESS        = 0;
+	var WREN_RESULT_COMPILE_ERROR  = 1;
+	var WREN_RESULT_RUNTIME_ERROR  = 2;
 } //WrenInterpretResult
 
 @:enum
 abstract WrenType(Int)
 from Int to Int {
-    var WREN_TYPE_BOOL        = 0;
-    var WREN_TYPE_NUM         = 1;
-    var WREN_TYPE_FOREIGN     = 2;
-    var WREN_TYPE_LIST        = 3;
-    var WREN_TYPE_NULL        = 4;
-    var WREN_TYPE_STRING      = 5;
-    var WREN_TYPE_UNKNOWN     = 6;
+	var WREN_TYPE_BOOL        = 0;
+	var WREN_TYPE_NUM         = 1;
+	var WREN_TYPE_FOREIGN     = 2;
+	var WREN_TYPE_LIST        = 3;
+	var WREN_TYPE_NULL        = 4;
+	var WREN_TYPE_STRING      = 5;
+	var WREN_TYPE_UNKNOWN     = 6;
 } //WrenType
